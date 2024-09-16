f3d33bab75ceac014fddd99ebe0a536445ddc085
Leticia Calabria <leticiacalabria1@hotmail.com>
2024-09-16 17:42:09 +0100

    fix: adjusting the calc

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 function calculateLoan() {
 
-    loanAmountEl = document.getElementById("loan-amount").value;
-    interestRatelEl = document.getElementById("interest-rate").value;
-    monthsEl = document.getElementById("months").value;
+    let loanAmountEl = parseFloat(document.getElementById("loan-amount").value);
+    let interestRatelEl = parseFloat(document.getElementById("interest-rate").value);
+    let monthsEl = parseInt(document.getElementById("months").value);
     
-    interest = (loanAmountEl * (interestRatelEl * 0.01)) / monthsEl;
-    payment = (loanAmountEl / monthsEl + interest).toFixed(2);
+    let monthlyInterestRate = (interestRatelEl / 100) / 12;
+    let payment = (loanAmountEl * monthlyInterestRate * Math.pow(1 + monthlyInterestRate, monthsEl)) /
+                  (Math.pow(1 + monthlyInterestRate, monthsEl) - 1);
     
-    document.getElementById("payment").innerHTML = `Monthly Payment: ${payment}`;
+    document.getElementById("payment").innerHTML = `Monthly Payment: ${payment.toFixed(2)}`;
 }
 
 
