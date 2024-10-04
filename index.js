15c5b5f9896e977147867aede2c3c5362811da0e
Leticia Calabria <leticiacalabria1@hotmail.com>
2024-10-04 23:35:37 +0100

    feat: tip calculator

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 function calculateLoan() {
 
     let loanAmountEl = parseFloat(document.getElementById("loan-amount").value);
-    let interestRatelEl = parseFloat(document.getElementById("interest-rate").value);
+    let interestRateEl = parseFloat(document.getElementById("interest-rate").value);
     let monthsEl = parseInt(document.getElementById("months").value);
     
-    let monthlyInterestRate = (interestRatelEl / 100) / 12;
+    let monthlyInterestRate = (interestRateEl / 100) / 12;
     let payment = (loanAmountEl * monthlyInterestRate * Math.pow(1 + monthlyInterestRate, monthsEl)) /
                   (Math.pow(1 + monthlyInterestRate, monthsEl) - 1);
     
     document.getElementById("payment").innerHTML = `Monthly Payment: ${payment.toFixed(2)}`;
+}
+
+const btnEl = document.querySelector(".btn");
+
+function calculateTip() {
+
+    let billAmountEl = parseFloat(document.getElementById("bill-amount").value);
+    let tipPercentageEl = parseFloat(document.getElementById("tip-percentage").value);
+
+    let tipTotal = billAmountEl + (billAmountEl * (tipPercentageEl / 100));
+    
+    document.getElementById("tip-payment").innerHTML = `Total: ${tipTotal.toFixed(2)}`;
 }
 
 
